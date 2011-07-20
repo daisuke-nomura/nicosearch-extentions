7e54cfca1ac2e15fa5b7cd59873362fd31da8d65
tana <tana.laevatein@gmail.com>
2011-07-20 21:03:46 +0900

    タグ編集に対応、マイページに対応、隠れるバグを修正、タグ編集対応用に内部を変更

--- chrome/cs.js
+++ chrome/cs.js
@@ -3,15 +3,24 @@
 // 変換中に全てがひらがなの時に出ない問題を修正 ?????
 // マウスクリックで入力ではなく検索できるようにする done?
 // 場当たり的な対処が多いのでニコニコの仕様変更に対応できない可能性が高い
+var values = []; // 入力欄ごとのグローバル変数の代わり
+// input,inputvalueなどを使っている部分を全部valuesの中身を使うようにする
+/*
 var input = null;
 var inputvalue = null;
 var selected = null; // 現在選択されている候補
 var suggestlength = null; //現在表示されている候補の数
+*/
 var page = null; // ページの種類
+var tag_event_added = false; // タグ編集のイベントが追加されていればtrue
+var inputcount = 0; // 入力欄の番号
 // 読み込み
 function loadJSON(str, cb) {
   chrome.extension.sendRequest({name: "loadJSON", str: str}, cb);
 }
+/*function loadJSON(str, cb) {
+  cb(["あああ", "いいい", "ううう"]);
+}*/
 
 // parentNodeをたどってformになるのを探す submit用
 function formElem(elem) {
@@ -38,29 +47,31 @@ function all_space(str) {
   return true;
 }
 // 候補表示
-function show_suggest(res) {
+function show_suggest(res, id) {
+  vals = values[id]
   // 候補が無い場合は前の結果を残す
   if (res == [] || res == false) {
     return;
   }
-  selected = null;
-  var oldElem = document.getElementById("suggestelem");
+  vals.selected = null;
+  var oldElem = document.getElementById("suggestelem_" + id);
   if (oldElem != null) {
     document.body.removeChild(oldElem);
   }
-  if (input.value == "" || all_space(input.value)) {
+  if (vals.input.value == "" || all_space(vals.input.value)) {
     return;
   }
   var suggestElem = document.createElement("div");
-  suggestElem.id = "suggestelem";
-  var inputpos = pos(input);
+  suggestElem.id = "suggestelem_" + id;
+  suggestElem.className = "suggestbox";
+  var inputpos = pos(vals.input);
   suggestElem.style.position = "absolute";
   //suggestElem.style.paddingLeft = "3px";
   suggestElem.style.left = "" + (inputpos.left - 3) + "px";
   //suggestElem.style.left = "" + (inputpos.left) + "px";
-  suggestElem.style.top = "" + (inputpos.top + input.offsetHeight + 2) + "px";
+  suggestElem.style.top = "" + (inputpos.top + vals.input.offsetHeight + 2) + "px";
   suggestElem.style.backgroundColor = "#ffffff";
-  suggestElem.style.minWidth = "" + input.offsetWidth + "px";
+  suggestElem.style.minWidth = "" + vals.input.offsetWidth + "px";
   suggestElem.style.textAlign = "left";
   for (var i = 0; i < res.length; i++) {
     var p = document.createElement("p");
@@ -68,9 +79,13 @@ function show_suggest(res) {
     //p.style.display = "block";
     // クリックした時
     p.addEventListener("click", function(e) {
-      input.value = e.target.firstChild.nodeValue;
-      document.body.removeChild(document.getElementById("suggestelem"));
+      vals.input.value = e.target.firstChild.nodeValue;
+      document.body.removeChild(document.getElementById("suggestelem_" + id));
       var form = formElem(input);
+      // タグ編集用
+      if (vals.input.name == "tag") {
+        return;
+      }
       // トップページ用
       //if (form.action != "") {
       if (page == "top") {
@@ -87,6 +102,11 @@ function show_suggest(res) {
       // 静画用
       if (document.location.host == "seiga.nicovideo.jp") {
         document.getElementById("search_button").click();
+        return;
+      }
+      // マイページ用
+      if (page == "my") {
+        document.getElementsByClassName("miniSearchSubmit")[0].click();
         return;
       }
       // 動画関係のページやその他用
@@ -119,36 +139,65 @@ function show_suggest(res) {
   link.appendChild(document.createTextNode("Powered by nicotools.com"));
   suggestElem.appendChild(link);
   document.body.appendChild(suggestElem);
-  suggestlength = res.length;
+  vals.suggestlength = res.length;
 }
 // 上下が押された時 upがtrueなら上
-function updown(up) {
+function updown(up, id) {
   up = !up;
-  var suggestElem = document.getElementById("suggestelem");
-  var nextElem = suggestElem.childNodes(selected + 1);
-  if (selected == null) {
-    selected = 0;
+  vals = values[id];
+  if (vals.selected == null) {
+    vals.selected = 0;
+  }
+  var suggestElem = document.getElementById("suggestelem_" + id);
+  var nextElem = suggestElem.childNodes(vals.selected + 1);
+  if (vals.selected == null) {
+    vals.selected = 0;
   } else if (up) {
-    selected += 1;
+    vals.selected += 1;
   } else {
-    selected -= 1;
+    vals.selected -= 1;
   }
-  if (selected < 0) {
-    selected = suggestlength - 1;
+  if (vals.selected < 0) {
+    vals.selected = vals.suggestlength - 1;
     suggestElem.childNodes(0).style.backgroundColor = "#ffffff";
-  } else if (selected >= suggestlength) {
-    selected = 0;
+  } else if (vals.selected >= vals.suggestlength) {
+    vals.selected = 0;
   }
-  if (up && suggestElem.childNodes(selected - 1)) {
-    suggestElem.childNodes(selected - 1).style.backgroundColor = "#ffffff";
-  } else if (suggestElem.childNodes(selected + 1) && nextElem.tagName != "A") {
-    suggestElem.childNodes(selected + 1).style.backgroundColor = "#ffffff";
+  if (up && suggestElem.childNodes(vals.selected - 1)) {
+    suggestElem.childNodes(vals.selected - 1).style.backgroundColor = "#ffffff";
+  } else if (suggestElem.childNodes(vals.selected + 1) && nextElem.tagName != "A") {
+    suggestElem.childNodes(vals.selected + 1).style.backgroundColor = "#ffffff";
   }
-  if (selected != suggestlength - 1) {
-    suggestElem.childNodes(suggestlength - 1).style.backgroundColor = "#ffffff";
+  if (vals.selected != vals.suggestlength - 1) {
+    suggestElem.childNodes(vals.suggestlength - 1).style.backgroundColor = "#ffffff";
   }
-  suggestElem.childNodes(selected).style.backgroundColor = "#afeeee";
-  input.value = suggestElem.childNodes(selected).innerHTML;
+  suggestElem.childNodes(vals.selected).style.backgroundColor = "#afeeee";
+  vals.input.value = suggestElem.childNodes(vals.selected).innerHTML;
+}
+// DOM要素inputでサジェストを有効化
+function makesuggest(input) {
+  var id = inputcount.toString();
+
+  values[id] = {};
+  values[id]["input"] = null;
+  values[id]["inputvalue"] = null;
+  values[id]["selected"] = null; // 現在選択されている候補
+  values[id]["suggestlength"] = null; //現在表示されている候補の数
+  values[id]["input"] = input;
+
+  input.addEventListener("keydown", function(e) {
+    if (e.keyCode == 38) {
+      updown(true, id);
+      return;
+    } else if (e.keyCode == 40) {
+      updown(false, id);
+      return;
+    }
+    inputvalue = input.value;
+    loadJSON(input.value, function (arr) { show_suggest(arr, id); });
+  });
+  input.setAttribute("autocomplete", "off");
+  inputcount = inputcount + 1;
 }
 
 function main() {
@@ -175,10 +224,31 @@ function main() {
     page = "dic";
     input = document.getElementsByName("q")[0];
   }
+  // マイページ
+  if (input == null) {
+    page = "my";
+    input = document.getElementsByClassName("miniSearchWord")[0];
+  }
+  // タグ編集 tag_editを監視
+  // 複数の入力欄に対処できない!!!!! 完了
+  if (page == "video") {
+    var tagedit = document.getElementsByClassName("tag_edit")[0];
+    //console.log(tagedit);
+    if (tagedit != [] && tagedit != undefined) {
+      tagedit.addEventListener("DOMSubtreeModified", function() {
+        if (!tag_event_added) {
+          makesuggest(document.getElementsByName("tag")[0]);
+          tag_event_added = true;
+        }
+      });
+    }
+  }
   // 入力欄が無い場合は何もしない
   if (input == null) {
     return;
   }
+  makesuggest(input);
+  /*
   input.addEventListener("keydown", function(e) {
     if (e.keyCode == 38) {
       updown(true);
@@ -191,6 +261,7 @@ function main() {
     loadJSON(input.value, show_suggest);
   });
   input.setAttribute("autocomplete", "off");
+  */
 }
 // 生放送でloadが動かない!!!!!!!!!!!!!!!!!!
 // ↑ 解決 manifest.jsonでrun_atを指定した 動かなかった原因は不明
@@ -201,12 +272,15 @@ main();
 document.body.addEventListener("click", function(e) {
   x = e.x;
   y = e.y;
-  var suggestElem = document.getElementById("suggestelem");
-  if (suggestElem == null) {
-    return;
-  }
-  var rect = suggestElem.getBoundingClientRect();
-  if (!(x > rect.left && y > rect.top && x < rect.right && y < rect.bottom)) {
-    document.body.removeChild(suggestElem);
+  // 修正必要 done
+  for (var i = 0; i < values.length; i++) {
+    var suggestElem = document.getElementById("suggestelem_" + i.toString());
+    if (suggestElem == null) {
+      return;
+    }
+    var rect = suggestElem.getBoundingClientRect();
+    if (!(x > rect.left && y > rect.top && x < rect.right && y < rect.bottom)) {
+      document.body.removeChild(suggestElem);
+    }
   }
 });
